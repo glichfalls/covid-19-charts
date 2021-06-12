87a4808aa9dc323a3fd65fc3b91e5d5ebf622c58
Robin Portner <robin.portner@hotmail.com>
2021-06-12 10:13:32 +0200

    added 2 classes

---
+++ models/test.py
@@ -0,0 +1,10 @@
+
+
+class Test:
+
+    def __init__(self, country: int, total_vaccinations: int, people_vaccinated: int, people_fully_vaccinated: int, new_vaccinations: int):
+        self._country = country
+        self._total_vaccinations = total_vaccinations
+        self._people_vaccinated = people_vaccinated
+        self._people_fully_vaccinated = people_fully_vaccinated
+        self._new_vaccinations = new_vaccinations
