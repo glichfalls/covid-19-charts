b40c15b17d5bb90791afbab790fb0ef520742c62
Marino Bantli <marino@maban.ch>
2021-06-13 11:46:20 +0200

    Implemented Case- and Deathchart, added formatting

--- charts.py
+++ charts.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import matplotlib.dates as mdates
 from db import DatabaseConnection
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class Charts:
@@ -37,3 +37,37 @@ class Charts:
         plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
         plt.grid(True)
         plt.show()
+
+    def show_case_death_chart(self):
+        dbData = self._db.get_total_cases()
+
+        date = [datetime.strptime(x[0], '%Y-%m-%d') for x in dbData]
+        cases = [x[1] for x in dbData]
+        deaths = [x[2] for x in dbData]
+
+        fig, ax1 = plt.subplots()
+
+        color = 'tab:red'
+        ax1.set_xlabel('Datum')
+        ax1.set_ylabel('Fälle', color=color)
+        ax1.plot(date, cases, color=color)
+        ax1.tick_params(axis='y', labelcolor=color)
+
+        plt.gca().yaxis.set_major_formatter(ticker.EngFormatter())
+
+        ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+        color = 'tab:blue'
+        ax2.set_ylabel('Gestorbene', color=color)  # we already handled the x-label with ax1
+        ax2.plot(date, deaths, color=color)
+        ax2.tick_params(axis='y', labelcolor=color)
+
+        plt.gcf().autofmt_xdate()
+        plt.gca().yaxis.set_major_formatter(ticker.EngFormatter())
+        plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=2))
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+        plt.grid(True)
+
+        fig.tight_layout()  # otherwise the right y-label is slightly clipped
+        plt.show()
+
